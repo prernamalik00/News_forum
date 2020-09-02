c18d5beab18384f57221e00d3dddfa5e17d1f15d
Manav802 <manavchhabrj7@gmail.com>
2020-09-03 01:14:44 +0530

    Header and Footer layout

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div class="sticky-top header">
+            <div className="d-flex user-header">
+                HII
+            </div>
+            <div className="d-flex navigation">
+                Byeee
+            </div>
+        </div>
+    )
+}
+export default Header
--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/main.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById("root")
 );
