a04de48e6eb4ca64a2a2c025d6181e2a728cce09
srishtytakyar <51585827+srishtytakyar@users.noreply.github.com>
2020-09-27 13:11:05 +0530

    updated

--- app/src/components/HorizontalCards.jsx
+++ app/src/components/HorizontalCards.jsx
@@ -3,27 +3,18 @@ import React from "react";
 import Card from "./RecentSecCard";
 
 function horizontalcards() {
-    return ( <
-        div className = "card-columns" >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        Card / >
-        <
-        /div>
-    );
+  return (
+    <div className="card-columns">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 }
 
 export default horizontalcards;
