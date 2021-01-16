7ea513aaa33d96a3af49b3c952f219e0b0b62519
srishtytakyar <51585827+srishtytakyar@users.noreply.github.com>
2021-01-17 04:25:12 +0530

    Updates

---
+++ precache-manifest.ed0a256867d1695fb9b55535a3ab46ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "483e01f5f8ed049bafcc0531e7ad4d6f",
+    "url": "/News_forum/index.html"
+  },
+  {
+    "revision": "4013cd25f2fa7b24518f",
+    "url": "/News_forum/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "cedeac4a19b79e3e45b4",
+    "url": "/News_forum/static/css/main.d2e1e5fb.chunk.css"
+  },
+  {
+    "revision": "4013cd25f2fa7b24518f",
+    "url": "/News_forum/static/js/2.ae0bea36.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/News_forum/static/js/2.ae0bea36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedeac4a19b79e3e45b4",
+    "url": "/News_forum/static/js/main.1d0a8c91.chunk.js"
+  },
+  {
+    "revision": "154074a7ad49cfc9ad7e",
+    "url": "/News_forum/static/js/runtime-main.cb3c11b3.js"
+  },
+  {
+    "revision": "c9f309b3d47969ecac64a77a6c672594",
+    "url": "/News_forum/static/media/Nexa Bold.c9f309b3.otf"
+  },
+  {
+    "revision": "12108809f49c49bcdc126dcecc938761",
+    "url": "/News_forum/static/media/Nexa Light.12108809.otf"
+  },
+  {
+    "revision": "d95152ab5a160491d28b3fce25bf4ff2",
+    "url": "/News_forum/static/media/NunitoSans-Black.d95152ab.ttf"
+  },
+  {
+    "revision": "08e53a516d2ba719d98da46c49b3c369",
+    "url": "/News_forum/static/media/NunitoSans-Bold.08e53a51.ttf"
+  },
+  {
+    "revision": "505a059580cfbeaccdcb7a489bb67ec9",
+    "url": "/News_forum/static/media/NunitoSans-ExtraBold.505a0595.ttf"
+  },
+  {
+    "revision": "4c8f447011eef80831b45edb1e5971e0",
+    "url": "/News_forum/static/media/NunitoSans-Regular.4c8f4470.ttf"
+  },
+  {
+    "revision": "bd318b58018198a57723f311627492ac",
+    "url": "/News_forum/static/media/NunitoSans-SemiBold.bd318b58.ttf"
+  }
+]);
